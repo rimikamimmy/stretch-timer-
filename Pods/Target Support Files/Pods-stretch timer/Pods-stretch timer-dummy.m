f2c18ec9ffa2b715efb2c8739c69017715e2b91d
RIMIKA <rimikamimmy727@gmail.com>
2018-05-09 22:27:20 +0900

    タイマーきちんと動くようになりました！

---
+++ Pods/Target Support Files/Pods-stretch timer/Pods-stretch timer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_stretch_timer : NSObject
+@end
+@implementation PodsDummy_Pods_stretch_timer
+@end
---
+++ Pods/Target Support Files/Pods-stretch timerUITests/Pods-stretch timerUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_stretch_timerUITests : NSObject
+@end
+@implementation PodsDummy_Pods_stretch_timerUITests
+@end
