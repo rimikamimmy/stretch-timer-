7b7476e11ece773e51689ae727b50b7feb22c2c1
RIMIKA <rimikamimmy727@gmail.com>
2018-05-09 22:26:13 +0900

    タイマー動くようになりました！！

--- stretch timer/TimerViewController.swift
+++ stretch timer/TimerViewController.swift
@@ -2,7 +2,7 @@
 //  TimerViewController.swift
 //  stretch timer
 //
-//  Created by Rimika Inoguchi on 2018/04/27.
+//  Created by Rimika Inoguchi on 2018/05/09.
 //  Copyright © 2018年 Rimika Inoguchi. All rights reserved.
 //
 
@@ -10,71 +10,88 @@ import UIKit
 
 class TimerViewController: UIViewController {
     
-    
-    var timer = Timer()
-    var startTime: TimeInterval = 0
-    var elapsedTime = 0.0
-    var time  = 0.0
+    var StopWatchTimer = Timer()
+    var currentTime = 0
     
     
-    @IBOutlet var labelTimer: UILabel!
-    @IBOutlet var buttonStart: UIButton!
-    @IBOutlet var buttonStop: UIButton!
-    @IBOutlet var buttonReset: UIButton!
+    @IBOutlet weak var minutesLabel: UILabel!
+    @IBOutlet weak var secondsLabel: UILabel!
+    @IBOutlet weak var pauseButton: UIButton!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var modoruButton: UIButton!
     
-    @IBAction func startbuttonTapped() {
-        
-        buttonStart.isEnabled = false
-        buttonReset.isEnabled = false
-        
-        buttonStop.isEnabled = true
-        
-        timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector (self.update), userInfo: nil, repeats: true)
-    }
     
-    @IBAction func update() {
+    
+    @IBAction func hasPauseButtonPressed(_ sender: Any) {
+        startButton.isHidden = false
+        pauseButton.isHidden = true
         
-        time = Date().timeIntervalSince1970 - startTime + elapsedTime
-        
-        let sec = Int(time)
-        let msec = Int((time - Double(sec)) * 100)
-        
-        let displayStr = NSString(format: "%02d:%02d.%02d", sec/60, sec%60, msec) as String
-        labelTimer.text = displayStr
-        
-        
-    }
-    @IBAction func stopbuttonTapped() {
-        
-        buttonStart.isEnabled = true
-        buttonReset.isEnabled = true
-        
-        buttonStop.isEnabled = false
-        
-        timer.invalidate()
-        
-        elapsedTime = time
+        StopWatchTimer.invalidate()
+    
     }
     
     
-    @IBAction func resetbuttonTapeed() {
+    @IBAction func hasStartButtonPressed(_ sender: Any) {
         
+        startButton.isHidden = true
+        pauseButton.isHidden = false
+        stopButton.isEnabled = true
         
-        elapsedTime = 0.0
-        labelTimer.text = "00:00.00"
+        StopWatchTimer = Timer.scheduledTimer(timeInterval: 0.05, target: self, selector: (#selector(TimerViewController.updateTime)), userInfo: nil, repeats: true)
+        
     }
+    
+    @IBAction func hasStopButtonPressed(_ sender: Any) {
+        
+        pauseButton.isHidden = true
+        startButton.isHidden = false
+        stopButton.isEnabled = false
+        
+        currentTime = 0
+        minutesLabel.text = "0"
+        secondsLabel.text = "0"
+        
+        StopWatchTimer.invalidate()
+        
+    }
+    
+    @objc func updateTime(){
+        
+        currentTime += 1
+        minutesLabel.text = "\(currentTime / 60)"
+        secondsLabel.text = "\(currentTime % 60)"
+        
+        if currentTime == 3600{
+            currentTime = 0
+        }
+        
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        pauseButton.isHidden = true
+        stopButton.isEnabled = false
+    
         
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        
-        
+        // Dispose of any resources that can be recreated.
     }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
-
-
